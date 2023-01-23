9f35fedf88908f06d76d75b14be8b41ab50c1903
Stefan Alenius <darlanio@gmail.com>
2023-01-23 12:28:20 +0100

    Initial Commit - TensorFlow Net GAN for VS2022
    This is a non-working solution and I am wondering if the updated NuGet packages correctly connect the TrainableVariables or not.
    The example code is from the old Example project - if that has been updated, I will need to update this as well.

---
+++ TensorFlowNet-Updated-GAN/TensorFlowNet-Updated-GAN/Program.cs
@@ -0,0 +1,19 @@
+﻿// NuGetPackages:
+/*
+Install-Package SciSharp.Tensorflow.Redist-Windows-GPU
+Install-Package System.Drawing.Common
+Install-Package Tensorflow.Keras
+Install-Package Tensorflow.NET
+*/
+
+namespace TensorFlowNet_Updated_GAN
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            new MnistGAN().Run();            
+        }
+    }
+}
+
